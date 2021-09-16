85bf1632dc401d223e8939f25571785968bdef03
saltyposaune <ewalkertbone@gmail.com>
2021-09-15 19:50:58 -0600

    user seeds

---
+++ seeds/index.js
@@ -0,0 +1,12 @@
+const seedUsers = require('./user-seeds');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+    await seedUsers();
+    console.log('--------------');
+  
+    process.exit(0);
+  };
+
+  seedAll();
---
+++ seeds/user-seeds.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'swimteampie',
+        email: 'email@email.com',
+        password: 'password'
+    },
+    {
+        username: '$avatage',
+        email: 'email@email.com',
+        password: 'contrasena'
+    },
+    {
+        username: 'wingnut33',
+        email: 'email@email.com',
+        password: 'heslo'
+    },
+    {
+        username: 'goldbrick85',
+        email: 'email@email.com',
+        password: 'le-mot-de-passe'
+    },
+    {
+        username: 'potatocake93',
+        email: 'email@email.com',
+        password: "muw'lj"
+    },
+]
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
