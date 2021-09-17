2ebbb0b2a572257e0b2b0df101ff9a4cbbd70335
Dylan Shoemaker <me@dylanshoemaker.com>
2021-09-17 12:00:40 -0700

    complete index.js for seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,22 +1,26 @@
 const seedUsers = require('./user-seeds');
+const seedPlayers = require('./player-seeds');
+const seedParties = require('./party-seeds');
+const seedEnemies = require('./enemy-seeds');
+
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
-    console.log('--------------');
+    console.log('------ DATABASE SYNCED --------');
 
     await seedUsers();
-    console.log('--------------');
+    console.log('------ USERS SEEDED --------');
 
     await seedPlayers();
-    console.log('--------------');
+    console.log('------- PLAYERS SEEDED -------');
 
     await seedParties();
-    console.log('--------------');
+    console.log('------- PARTIES SEEDED -------');
 
-    await seedMonsters();
-    console.log('--------------');
+    await seedEnemies();
+    console.log('------- ENEMIES SEEDED -------');
   
     process.exit(0);
   };
