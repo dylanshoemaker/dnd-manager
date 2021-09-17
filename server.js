471d20625a22c8839859c841d83b828db95e2150
Trent Roesbery <xtrent777x@gmail.com>
2021-09-17 16:06:39 -0600

    express session and sequalize store package for cookies

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'dnd manager',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
