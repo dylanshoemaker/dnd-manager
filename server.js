c8ae88c1617f036f2022f74569de12de9d5a3773
jessan <jessenia.garcia998@gmail.com>
2021-09-16 21:20:46 -0600

    handlebars initial set up following the npm documentation

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+var exphbs = require('express-handlebars')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//handlebars GET
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 // turn on routes
 app.use(routes);
@@ -15,3 +25,6 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+//handlebars port, for testing
+app.listen(3000);
