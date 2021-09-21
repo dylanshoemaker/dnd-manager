c60e3fd432fae6b1768e68230f1c82d48f90597e
saltyposaune <ewalkertbone@gmail.com>
2021-09-20 20:33:46 -0600

    tweaks

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,5 @@ $(document).ready(function () {
 });
 
 
-<<<<<<< HEAD:assets/script.js
-=======
 
 // profileImg.addEventListener("click", showImg);
->>>>>>> 40139f7f62200c38734d41b1cae0d53f7c5aaa87:public/js/index.js
