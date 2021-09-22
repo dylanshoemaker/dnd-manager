05d8d827262232e51f72759be54509ec1560c5b0
saltyposaune <ewalkertbone@gmail.com>
2021-09-21 19:48:03 -0600

    login js

--- public/js/index.js
+++ public/js/index.js
@@ -2,33 +2,4 @@ $(document).ready(function () {
     $('.carousel').carousel();
 });
 
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
 
