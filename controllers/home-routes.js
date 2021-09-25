91dc8050619f0d72ce7dc242af1f28ed1cf5eed3
jessan <jessenia.garcia998@gmail.com>
2021-09-24 22:25:07 -0600

    added last handlebar page, fixing homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { Party } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', party);
 });
 
 router.get('/login', (req, res) => {
@@ -13,5 +13,9 @@ router.get('/login', (req, res) => {
 router.get('/createaccount', (req, res) => {
     res.render('createaccount');
 });
+
+router.get('/player', (req, res) => {
+    res.render('player')
+})
 
 module.exports = router;
