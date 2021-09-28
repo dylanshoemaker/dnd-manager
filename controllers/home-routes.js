56a20c81f71e66f88f6de21b5699d3f1c70e07f9
jessan <jessenia.garcia998@gmail.com>
2021-09-28 01:08:19 -0600

    issues with enemy array being called through routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,50 +6,56 @@ const { Enemy } = require('../models');
 
 router.get('/', (req, res) => {
     Party.findAll({
-        
+
     })
         .then((dbPartyData) => {
             const partyData = dbPartyData.map(party => party.get({
                 plain: true
             }))
-            console.log("partyData");
-            console.log(partyData);
             res.render('homepage', {
                 partyData,
                 loggedIn: req.session.loggedIn
             })
         });
 });
+
 router.get('/player', (req, res) => {
     Player.findAll({
-        
+
     })
         .then((dbPlayerData) => {
             const playerData = dbPlayerData.map(player => player.get({
                 plain: true
             }))
-            console.log("playerData");
-            console.log(playerData);
+            const enemyData = dbEnemyData.map(enemy => enemy.get({
+                plain: true
+            }))
             res.render('player', {
                 playerData,
-                loggedIn: req.session.loggedIn
+                enemyData,
+                loggedIn: req.session.loggedIn,
+                playerPage: req.session.playerPage
             })
-    });
+        });
 
+});
+
+router.get('/player', (req, res) => {
     Enemy.findAll({
-        
-        })
-            .then((dbEnemyData) => {
-                const enemyData = dbEnemyData.map(enemy => enemy.get({
-                    plain: true
-                }))
-                console.log("enemyData");
-                console.log(enemyData);
-                res.render('player', {
-                    enemyData,
-                    loggedIn: req.session.loggedIn
-                })
-    });
+
+    })
+        .then((dbEnemyData) => {
+            const enemyData = dbEnemyData.map(enemy => enemy.get({
+                plain: true
+            }))
+            console.log(enemyData);
+
+            res.render('player', {
+                enemyData,
+                loggedIn: req.session.loggedIn,
+                playerPage: req.session.playerPage
+            })
+        });
 });
 
 router.get('/login', (req, res) => {
