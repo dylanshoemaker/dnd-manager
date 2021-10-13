f15aed2768fdc1e34f2d5559a9eba7c574732f93
Matt <matt.white.624@gmail.com>
2021-10-13 17:29:12 -0600

    added enemy data to player page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,96 +1,79 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Party, Player, Enemy, User  } = require("../models");
+const {
+  Party,
+  Player,
+  Enemy,
+  User
+} = require("../models");
 
 
 router.get("/party", (req, res) => {
   User.findAll({
-    where: {
-      id: req.session.user_id
-    },
-    include: [{
-      model: Party,
-      attributes: ['id', 'party_name']
-    }]
-  })
+      where: {
+        id: req.session.user_id
+      },
+      include: [{
+        model: Party,
+        attributes: ['id', 'party_name']
+      }]
+    })
     .then((dbUserData) => {
       const dbPartyData = dbUserData[0].parties.map((user) =>
-      user.get({
-        plain: true,
-      })
-    )
-    // console.log(dbPartyData)
-    res.render("homepage", {
-      dbPartyData: dbPartyData,
-      loggedIn: req.session.loggedIn,
+        user.get({
+          plain: true,
+        })
+      )
+      // console.log(dbPartyData)
+      res.render("homepage", {
+        dbPartyData: dbPartyData,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
 });
 
 router.get("/party/:party_name", (req, res) => {
   Party.findOne({
-    where: {
-      party_name: req.params.party_name,
-    },
-    include: [{
-      model: Player
-    }],
-  })
-  .then((dbPartyData) => {
-    const dbPlayerData = dbPartyData.players.map((players) =>
-      players.get({
-        plain: true,
-      })
-    )
-    
-    // console.log(dbPlayerData);
-    // res.json(dbPlayerData);
-    res.render("player", {
-      dbPlayerData: dbPlayerData,
-      loggedIn: req.session.loggedIn,
-      party_name: req.params.party_name
+      where: {
+        party_name: req.params.party_name,
+      },
+      include: [{
+        model: Player,
+      },
+      {
+        model: Enemy,
+      }],
+    })
+    .then((dbPartyData) => {
+      // console.log(dbPartyData)
+      const dbPlayerData = dbPartyData.players.map((players) =>
+        players.get({
+          plain: true,
+        })
+      )
+      console.log(dbPlayerData);
+      const dbEnemyData = dbPartyData.enemies.map((enemies) =>
+        enemies.get({
+          plain: true,
+        })
+      )
+      console.log(dbEnemyData);
+      res.render("player", {
+        dbPlayerData: dbPlayerData,
+        dbEnemyData: dbEnemyData,
+        loggedIn: req.session.loggedIn,
+        party_name: req.params.party_name
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
 });
-
-// router.get("/party/:party_name", (req, res) => {
-//     Enemy.findOne({
-//       where: {
-//         party_name: req.params.party_name,
-//       },
-//       include: [{
-//         model: Enemy
-//       }],
-//     })
-//     .then((dbEnemyData) => {
-//       const enemyData = dbEnemyData.enemy.map((enemy) =>
-//         enemy.get({
-//           plain: true,
-//         })
-//       )
-      
-//       console.log(enemyData);
-//       // res.json(dbPlayerData);
-//       res.render("enemy", {
-//         enemyData: enemyData,
-//         loggedIn: req.session.loggedIn,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-
 
 router.get("/createaccount", (req, res) => {
   res.render("createaccount");
@@ -121,7 +104,8 @@ router.get("/createaccount", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
-  } res.render("createaccount");
+  }
+  res.render("createaccount");
 });
 
 module.exports = router;
